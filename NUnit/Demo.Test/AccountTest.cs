15725c8fa5fc732c5849cf3b96532f5f2715d975
pdk2580 <pdk2580@gmail.com>
2016-05-26 17:25:59 +0800

    NUnitDemo initial commit

---
+++ NUnit/Demo.Test/AccountTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Demo;
+
+namespace Demo.Test
+{
+    [TestFixture]
+    public class AccountTest
+    {
+        Account source;
+        Account destination;
+
+        [SetUp]
+        public void Init()
+        {
+            source = new Account();
+            source.Deposit(200m);
+
+            destination = new Account();
+            destination.Deposit(150m);
+        }
+
+        [Test]
+        public void TransferFunds()
+        {
+            source.TransferFunds(destination, 100m);
+
+            Assert.AreEqual(250m, destination.Balance);
+            Assert.AreEqual(100m, source.Balance);
+        }
+
+        [Test]
+        public void TransferWithInsufficientFunds()
+        {
+            Assert.Throws<InsufficientFundsException>(()=> source.TransferFunds(destination, 300m));
+        }
+
+        [Test]
+        [Ignore("Decide how to implement transaction management")]
+        public void TransferWithInsufficientFundsAtomicity()
+        {
+            try
+            {
+                source.TransferFunds(destination, 300m);
+            }
+            catch (InsufficientFundsException expected)
+            {
+            }
+
+            Assert.AreEqual(200m, source.Balance);
+            Assert.AreEqual(150m, destination.Balance);
+        }
+    }
+}
